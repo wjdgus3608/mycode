e44c6fcf2a98fd5d60707ae239ce0811262b38fa
wjdgus3608 <31335823+wjdgus3608@users.noreply.github.com>
2021-10-08 14:11:17 +0900

    돌 게임 - 게임 이론

---
+++ 9655.java
@@ -0,0 +1,27 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class Main {
+	static int N;
+	static boolean isImpossible;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		
+		System.out.println(N%2==0?"CY":"SK");
+	}
+	
+}
+
+
+
+
